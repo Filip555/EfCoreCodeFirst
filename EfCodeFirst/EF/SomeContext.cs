36144a96c1d2254b86c428705ba01a9db68f34dc
Pawel Filipek <pawel.fil555@gmail.com>
2018-03-29 19:25:31 +0200

    fc

---
+++ EfCodeFirst/EF/SomeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCodeFirst.EF
+{
+public class SomeContext : DbContext
+{
+    public DbSet<SomeTable> SomeTable { get; set; }
+    public SomeContext(DbContextOptions<SomeContext> options) : base(options)
+    {
+    }
+}
+}
